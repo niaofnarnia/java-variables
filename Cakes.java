a22c55f4e5e4f1ac596f13206f2c27fc8b2b4007
Nia <niaespinal@outlook.com>
2025-05-02 14:30:42 +0200

    Completed Cakes exercise

--- Cakes.java
+++ Cakes.java
@@ -7,29 +7,31 @@ public class Cakes {
 
         //Cuando dividimos dos enteros cabe la posibilidad de que el resultado sea una fracción, pero no está bien cambiar a double algo que conceptualmente es un entero (como el pastel o las personas)
         //Para que el resultado cambie de tipo usamos "type casting" y se hace agregando entre paréntesis lo que queremos convertir
-        
+
         //Corre el siguiente código
-        System.out.println("Cada persona se lleva " + ((double)cake / people) + " partes de pastel");
+        System.out.println("Cada persona se lleva " + ((double) cake / people) + " partes de pastel");
         //El resultado obtenido es de tipo double.
-        
+
 
         //Ahora vamos a escribir un programa que convierta un valor de tipo double a int y de int a double. 
 
         //Declara y asigna un valor a una variable double
+        double doubleValue = 13;
         //Escribe aquí tu código
-        System.out.println("Valor original de double: <doubleValue>");
+        System.out.println("Valor original de double: " + doubleValue);
 
         //Convierte double a int
         //Escribe aquí tu código
-        System.out.println("Valor convertido de double a int: <intValue>");
+        System.out.println("Valor convertido de double a int: " + (int) doubleValue);
 
         //Declara y asigna un valor a una variable int
+        int anotherIntValue = 31;
         //Escribe aquí tu código
-        System.out.println("Valor original de int: <anotherIntValue>");
+        System.out.println("Valor original de int: " + anotherIntValue);
 
         //Convierte int a double
         //Escribe aquí tu código
-        System.out.println("Valor convertido de int a double: <anotherDoubleValue>");
-               
+        System.out.println("Valor convertido de int a double: " + (double) anotherIntValue);
+
     }
 }
